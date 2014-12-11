b768d4e8fdbbdb3761bae31f8ba32ef9236dadbd
apple <apple@172-16-192-23.dynapool.nyu.edu>
2014-12-11 16:03:10 -0500

    third commit

--- python_project/testClass.py
+++ python_project/testClass.py
@@ -20,9 +20,66 @@ class TestStockClass(unittest.TestCase):
         print 'TestStockClass Over!'
 
 
-    def testName(self):
-        pass
+    def test_EmptyInput(self):
+        """
+        When some of the inputs are null, test whether it will raise the exception.
+        """
+        emptyinputlist = [('','',''),('','2010/1/1',''),('IBM','',''),('','2010/1/1','2010/5/1')]
+        for item in emptyinputlist:
+            stock_name,start_date,end_date = item
+            with self.assertRaises(EmptyInputException):
+                self.stockclass = Stock(stock_name,start_date,end_date)  
+    
+    def test_InvalidDate(self):
+        """
+        When the input of date is invalid, test whether it will raise the exception.
+        """
+        invaliddatelist = [('IBM','2010.1.1','2010.5.1'),('IBM','abc','de'),('IBM','a2010/1/1','201f/5/1')]
+        for item in invaliddatelist:
+            stock_name,start_date,end_date = item
+            with self.assertRaises(DateInputException):
+                self.stockclass = Stock(stock_name,start_date,end_date)  
+    
+    
+    def test_InvalidDateRange(self):
+        """
+        When the end date is before the start date, test whether it will raise the exception.
+        """
+        invaliddatelist = [('IBM','2010/1/1','2009/5/1'),('IBM','2010/3/3','2010/3/2')]
+        for item in invaliddatelist:
+            stock_name,start_date,end_date = item
+            with self.assertRaises(DateRangeException):
+                self.stockclass = Stock(stock_name,start_date,end_date) 
+    
+    def test_InvalidEndDate(self):
+        """
+        When the end date is beyond the current time, test whether it will raise the exception.
+        """
+        invaliddatelist = [('IBM','2010/1/1','2015/5/1'),('IBM','2010/3/3','2048/3/2')]
+        for item in invaliddatelist:
+            stock_name,start_date,end_date = item
+            with self.assertRaises(EndDateException):
+                self.stockclass = Stock(stock_name,start_date,end_date)   
 
+    def test_InvalidStockName(self):
+        """
+        When the stock name could not be found in the yahoo finance data between the start date and end date, 
+        test whether it will raise the exception.
+        """
+        invalidstocklist = [('11111','2010/1/1','2010/5/1'),('cba','2010/1/1','2010/5/1'),('&*ibm','2010/1/1','2010/5/1')]
+        for item in invalidstocklist:
+            stock_name,start_date,end_date = item
+            with self.assertRaises(StockNameInputException):
+                self.stockclass = Stock(stock_name,start_date,end_date)   
+
+    def test_ValidStock(self):
+        """
+        When the input of date is invalid, test whether it will raise the exception.
+        """
+        validstocklist = [('IBM','2010/1/1','2010/5/1'),('ibm','2013/1/1','2014/1/1')]
+        for item in validstocklist:
+            stock_name,start_date,end_date = item
+            self.assertTrue(Stock(stock_name,start_date,end_date))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
